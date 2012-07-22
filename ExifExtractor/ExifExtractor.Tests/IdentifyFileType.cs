321ca50f42e382e9ad9301c24b230a4caf1a7a84
Nathan Chere <git@nathanchere.com.au>
2012-07-23 00:39:02 +0200

    Basic shell project with first failing tests

---
+++ ExifExtractor/ExifExtractor.Tests/IdentifyFileType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExifExtractor.Tests
+{
+    public class IdentifyFileTypeTests
+    {
+        [Fact]
+        public void IdentifyJpeg()
+        {
+            Assert.True(false);
+        }
+
+        public void IdentifyCanonCr2()
+        {
+            Assert.True(false);
+        }
+
+        public void IdentifyNikonNek()
+        {
+            Assert.True(false);
+        }
+
+        public void IdentifyDng()
+        {
+            Assert.True(false);
+        }
+    }
+}
---
+++ ExifExtractor/ExifExtractor/Common.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExifExtractor
+{
+    public enum FileTypeEnum
+    {
+        Unknown,
+        JPEG,
+        CR2, // Canon proprietary RAW format
+        NEF, // Nikon proprietary RAW format
+        DNG, // Adobe open RAW format ("Digital Negative")
+    }
+     
+}
